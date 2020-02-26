a0be9b4550fbc9988bbab52cd9daa393b7f3f132
Tehseen Afzal <abuashja@gmail.com>
2020-02-26 22:39:28 +0500

    Creact Transaction component for transaction list

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Transaction = ({ text, amount }) => {
+  const sign = amount < 0 ? "-" : "+";
+
+  return (
+    <li className={amount < 0 ? "minus" : "plus"}>
+      {text}{" "}
+      <span>
+        {sign}${Math.abs(amount)}
+      </span>
+      <button className="delete-btn">X</button>
+    </li>
+  );
+};
+
+export default Transaction;
