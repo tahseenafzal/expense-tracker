c603d9a66e43c7ea7105b7841e6a0aa6efea0b7d
Tehseen Afzal <abuashja@gmail.com>
2020-02-26 22:38:19 +0500

    Add context api functionality and map array with Transaction component

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>$400</span>
-          <button className="delete-btn">X</button>
-        </li>
+        {transactions.map(transaction => (
+          <Transaction
+            key={transaction.id}
+            text={transaction.text}
+            amount={transaction.amount}
+          />
+        ))}
       </ul>
     </>
   );
