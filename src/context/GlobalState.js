b409693babe19292c71078aaade645396b4400b4
Tehseen Afzal <abuashja@gmail.com>
2020-06-12 15:40:43 +0500

    Create delete transaction function

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -7,8 +7,8 @@ const initialState = {
     { id: 1, text: "Flower", amount: -20 },
     { id: 2, text: "Salary", amount: 300 },
     { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 }
-  ]
+    { id: 4, text: "Camera", amount: 150 },
+  ],
 };
 
 // Create context
@@ -18,8 +18,21 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  //Actions
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  };
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
